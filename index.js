67c1de483d705aeb1e15022afab75f26177358a4
Daniel <danielstarling4@gmail.com>
2019-03-18 15:44:52 -0700

    delete working

--- index.js
+++ index.js
@@ -38,14 +38,37 @@ server.get("/api/users/:id", (req, res) => {
 
   db.findById(id)
     .then(user => {
-      res.status(200).json(user);
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      }
     })
+
     .catch(error => {
       res
         .status(500)
-        .json({ message: "The user information could not be retrieved." });
+        .json({ error: "The user information could not be retrieved." });
     });
 });
+
+server.delete("/api/users/:id", (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(user => {
+      if (user) {
+        res.status(204).json(user);
+      } else {
+        res.status(404).json({ error });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ message: "The user could not be removed" });
+    });
+});
+
 // server.put("/api/users/:id", (req, res) => {
 //   const { id } = req.params;
 //   const changes = req.body;
@@ -62,16 +85,4 @@ server.get("/api/users/:id", (req, res) => {
 //       res.status(500).json({ message: "error posting the user" });
 //     });
 // });
-// server.delete("/api/users/:id", (req, res) => {
-//   const id = req.params.id;
-//   db.remove(id).then(deleted => {
-//     res
-//       .status(204)
-//       .end()
-//       .catch(error => {
-//         res.status(500).json({ message: "error retrieving hubs" });
-//       });
-//   });
-// });
-
 server.listen(8000, () => console.log("API running on port 8000"));
